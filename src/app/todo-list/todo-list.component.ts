d94454402c457374babd4dcc6ee0e5e1db052496
Gupta, Sunny <sunnyvales789@gmail.com>
2024-04-29 08:52:38 +0530

    update

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -5,29 +5,31 @@ import { ADD_TODO, REMOVE_TODO, TOGGLE_TODO } from '../actions';
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
-  styleUrls: ['./todo-list.component.scss']
+  styleUrls: ['./todo-list.component.scss'],
 })
 export class TodoListComponent implements OnInit {
-
   @select() todos;
   model: ITodo = {
     id: 0,
-    description: "",
-    responsible: "",
-    priority: "low",
-    isCompleted: false
+    description: '',
+    responsible: '',
+    priority: 'low',
+    isCompleted: false,
   };
-  constructor(private ngRedux: NgRedux<IAppState>) { }
-  ngOnInit() {
-  }
+  constructor(private ngRedux: NgRedux<IAppState>) {}
+  ngOnInit() {}
   onSubmit() {
-    this.ngRedux.dispatch({type: ADD_TODO, todo: this.model});
+    console.log('onSubmit');
+    this.ngRedux.dispatch({ type: ADD_TODO, todo: this.model });
   }
+  // means marked as checked
   toggleTodo(todo) {
+    console.log(todo, 'toggleTodo');
     this.ngRedux.dispatch({ type: TOGGLE_TODO, id: todo.id });
   }
   removeTodo(todo) {
-    this.ngRedux.dispatch({type: REMOVE_TODO, id: todo.id });
-  }
+    console.log('removeTodo');
 
+    this.ngRedux.dispatch({ type: REMOVE_TODO, id: todo.id });
+  }
 }
